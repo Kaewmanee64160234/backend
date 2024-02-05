1117c9ddaa88c2ee6abf3d529b8e40a3fbc431d1
Kaewmanee Marasri <64160234@go.buu.ac.th>
2024-02-05 10:19:34 +0700

    set up db for connect db 4 free

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'www.db4free',
+      port: 3600,
+      username: 'hotelcarifornai',
+      password: 'pass@1234',
+      database: 'hotelcarifornai',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
