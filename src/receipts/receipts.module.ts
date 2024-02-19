f4974c8307bab500eab78ff53fc1a35608a85df2
Kantinan Srikasem <64160040@go.buu.ac.th>
2024-02-19 11:19:53 +0700

    ReceiptException

--- src/receipts/receipts.module.ts
+++ src/receipts/receipts.module.ts
@@ -8,9 +8,11 @@ import { Customer } from 'src/customers/entities/customer.entity';
 import { Employee } from 'src/employees/entities/employee.entity';
 import { Room } from 'src/rooms/entities/room.entity';
 import { Roomtype } from 'src/roomtypes/entities/roomtype.entity';
+import { Promotion } from 'src/promotions/entities/promotion.entity';
+import { Activity } from 'src/activity/entities/activity.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Receipt, ReceiptDetail, Customer, Employee, Room,Roomtype])],
+  imports: [TypeOrmModule.forFeature([Receipt, ReceiptDetail, Customer, Employee, Room, Roomtype, Promotion, Activity])],
   controllers: [ReceiptsController],
   providers: [ReceiptsService]
 })
